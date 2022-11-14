69bba4a7c09431f49508505e1f564ec8fd6dee60
Jamie Murphy <hello@itsjamie.dev>
2022-11-14 14:08:40 -0800

    Hello World

---
+++ hell.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*
+ * god cannot exist
+ */
+
+#define a ;
+#define aa {
+#define aaa }
+#define aaaa (
+#define aaaaa )
+#define aaaaaa int
+#define aaaaaaa void
+#define aaaaaaaa return
+#define aaaaaaaaa main
+#define aaaaaaaaaa 0
+#define aaaaaaaaaaa printf
+
+#define b "H"
+#define bb "e"
+#define bbb "l"
+#define bbbb "o"
+#define bbbbb "W"
+#define bbbbbb "r"
+#define bbbbbbb "d"
+#define bbbbbbbb " "
+#define bbbbbbbbb "K"
+#define bbbbbbbbbb "i"
+#define bbbbbbbbbbb "m"
+#define c "\n"
+
+/* I hate myself */
+aaaaaa aaaaaaaaa aaaa aaaaaaa aaaaa
+aa
+  aaaaaaaaaaa aaaa
+    b bb bbb bbb bbbb bbbbbbbb
+    bbbbb bbbb bbbbbb bbb bbbbbbb
+  aaaaa a
+
+  aaaaaaaaaaa aaaa
+    c
+  aaaaa a
+
+  aaaaaaaaaaa aaaa
+    bbbbbbbbb bbbbbbbbbb bbb bbb
+    bbbbbbbb bbbbbbbbbbb bb
+  aaaaa a
+
+  aaaaaaaa aaaaaaaaaa a
+aaa
---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define a {
+#define aa }
+#define aaa int
+#define aaaa main
+#define aaaaa (
+#define aaaaaa )
+#define aaaaaaa return
+#define aaaaaaaa ;
+#define aaaaaaaaa 0
+#define aaaaaaaaaa printf
+#define aaaaaaaaaaa "H"
+#define aaaaaaaaaaaa "e"
+#define aaaaaaaaaaaaa "l"
+#define aaaaaaaaaaaaaa "o"
+#define aaaaaaaaaaaaaaa "W"
+#define aaaaaaaaaaaaaaaa "r"
+#define aaaaaaaaaaaaaaaaa "d"
+#define aaaaaaaaaaaaaaaaaa " "
+#define aaaaaaaaaaaaaaaaaaa void
+
+aaa aaaa aaaaa aaaaaaaaaaaaaaaaaaa aaaaaa
+a
+  aaaaaaaaaa aaaaa aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa \
+  aaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaaaaa \
+  aaaaaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaa aaaaaaaa
+  aaaaaaa aaaaaaaaa aaaaaaaa
+aa
